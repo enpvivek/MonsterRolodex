70073cbf3dc865388f4903dfe69c5682bb007363
Vivek Yadav <enpvivek@gmail.com>
2022-06-17 02:01:53 +0530

    Added styles to website

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1> Monsters Rolodex</h1>
         <SearchBox
           placeholder="Search Monster"
           handleChange={(e) => {
